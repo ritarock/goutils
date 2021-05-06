71b463a90715c41c93b9457609219abc95f798cc
ritarock <dvryz1204@gmail.com>
2021-05-06 17:39:14 +0900

    update

--- src/array/array_utils.go
+++ src/array/array_utils.go
@@ -2,75 +2,116 @@ package array
 
 import "sort"
 
-func ToFloat64(array interface{}) []float64 {
-	var result []float64
+func SortArray(array interface{}) interface{} {
+	switch array.(type) {
+	case []int:
+		sort.Sort(sort.IntSlice(array.([]int)))
+	case []float64:
+		sort.Sort(sort.Float64Slice(array.([]float64)))
+	}
+
+	return array
+}
+
+func ReverseSortArray(array interface{}) interface{} {
+	switch array.(type) {
+	case []int:
+		sort.Sort(sort.Reverse(sort.IntSlice(array.([]int))))
+	case []float64:
+		sort.Sort(sort.Reverse(sort.Float64Slice(array.([]float64))))
+	}
+	return array
+}
+
+func UniqueArray(array interface{}) interface{} {
+	var newList interface{}
 
 	switch array.(type) {
-	case []float64:
-		result = array.([]float64)
 	case []int:
+		m := make(map[int]struct{})
+		newList = make([]int, 0)
+
 		for _, v := range array.([]int) {
-			result = append(result, float64(v))
+			if _, ok := m[v]; !ok {
+				m[v] = struct{}{}
+				newList = append(newList.([]int), v)
+			}
 		}
-	default:
-		result = append(result, 0)
-	}
-	return result
-}
+	case []float64:
+		m := make(map[float64]struct{})
+		newList = make([]float64, 0)
 
-func MaxOfArray(array []float64) float64 {
-	max := array[0]
-	for _, v := range array {
-		if max < v {
-			max = v
-		}
-	}
-	return max
-}
-
-func MinOfArray(array []float64) float64 {
-	min := array[0]
-	for _, v := range array {
-		if v < min {
-			min = v
-		}
-	}
-	return min
-}
-
-func SumOfArray(array []float64) float64 {
-	var sum float64 = 0
-	for _, v := range array {
-		sum += v
-	}
-	return sum
-}
-
-func AvarageOfArray(array []float64) float64 {
-	sumOfArray := SumOfArray(array)
-	return sumOfArray / float64(len(array))
-}
-
-func SortArray(array []float64) []float64 {
-	sort.Sort(sort.Float64Slice(array))
-	return array
-}
-
-func ReverseSortArray(array []float64) []float64 {
-	sort.Sort(sort.Reverse(sort.Float64Slice(array)))
-	return array
-}
-
-func UniqueArray(array []float64) []float64 {
-	m := make(map[float64]struct{})
-	newList := make([]float64, 0)
-
-	for _, v := range array {
-		if _, ok := m[v]; !ok {
-			m[v] = struct{}{}
-			newList = append(newList, v)
+		for _, v := range array.([]float64) {
+			if _, ok := m[v]; !ok {
+				m[v] = struct{}{}
+				newList = append(newList.([]float64), v)
+			}
 		}
 	}
 
 	return newList
+}
+
+func MaxOfArray(array interface{}) interface{} {
+	var max interface{}
+	arr := ReverseSortArray(array)
+
+	switch array.(type) {
+	case []int:
+		max = arr.([]int)[0]
+	case []float64:
+		max = arr.([]float64)[0]
+	}
+
+	return max
+}
+
+func MinOfArray(array interface{}) interface{} {
+	var min interface{}
+	arr := SortArray(array)
+
+	switch array.(type) {
+	case []int:
+		min = arr.([]int)[0]
+	case []float64:
+		min = arr.([]float64)[0]
+	}
+
+	return min
+}
+
+func SumOfArray(array interface{}) interface{} {
+	var sum interface{}
+
+	switch array.(type) {
+	case []int:
+		var s int
+		for _, v := range array.([]int) {
+			s += v
+		}
+		sum = s
+	case []float64:
+		var s float64
+		for _, v := range array.([]float64) {
+			s += v
+		}
+		sum = s
+	}
+
+	return sum
+}
+
+func AvarageOfArray(array interface{}) interface{} {
+	var avarage interface{}
+
+	sumOfArray := SumOfArray(array)
+
+	switch array.(type) {
+	case []int:
+		avarage = sumOfArray.(int) / len(array.([]int))
+	case []float64:
+		avarage = sumOfArray.(float64) / float64(len(array.([]float64)))
+	}
+
+	return avarage
 }
--- src/array/array_utils_test.go
+++ src/array/array_utils_test.go
@@ -5,80 +5,205 @@ import (
 	"testing"
 )
 
-var array []int = []int{1, 2, 3}
-
-func TestToFloat64(t *testing.T) {
-	intArray := []int{1, 2, 3}
-	result := ToFloat64(intArray)
-	expect := []float64{1, 2, 3}
-
-	if !reflect.DeepEqual(result, expect) {
-		t.Errorf("reult = %v, want %v", result, expect)
-	}
-}
-
-func TestMaxOfArray(t *testing.T) {
-	result := MaxOfArray(ToFloat64(array))
-	var expect float64 = 3
-
-	if result != expect {
-		t.Errorf("result = %v, want = %v", result, expect)
-	}
-}
-
-func TestMinOfArray(t *testing.T) {
-	result := MinOfArray(ToFloat64(array))
-	var expect float64 = 1
-
-	if result != expect {
-		t.Errorf("result = %v, want = %v", result, expect)
-	}
-}
-
-func TestSumOfArray(t *testing.T) {
-	result := SumOfArray(ToFloat64(array))
-	var expect float64 = 6
-
-	if result != expect {
-		t.Errorf("result = %v, want = %v", result, expect)
-	}
-}
-
-func TestAvarageOfArray(t *testing.T) {
-	result := AvarageOfArray(ToFloat64(array))
-	var expect float64 = 2
-
-	if result != expect {
-		t.Errorf("result = %v, want = %v", result, expect)
-	}
-}
-
 func TestSortArray(t *testing.T) {
-	array := []int{3, 1, 2, 10, 22}
-	result := SortArray(ToFloat64(array))
-	expect := []float64{1, 2, 3, 10, 22}
-
-	if !reflect.DeepEqual(result, expect) {
-		t.Errorf("reult = %v, want %v", result, expect)
+	type args struct {
+		array interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want interface{}
+	}{
+		{
+			name: "int array",
+			args: args{array: []int{5, 4, 3, 2, 1}},
+			want: []int{1, 2, 3, 4, 5},
+		},
+		{
+			name: "float array",
+			args: args{array: []float64{5, 4, 3, 2, 1}},
+			want: []float64{1, 2, 3, 4, 5},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := SortArray(tt.args.array); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("SortArray() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
 func TestReverseSortArray(t *testing.T) {
-	array := []int{3, 1, 2, 10, 22}
-	result := ReverseSortArray(ToFloat64(array))
-	expect := []float64{22, 10, 3, 2, 1}
-
-	if !reflect.DeepEqual(result, expect) {
-		t.Errorf("reult = %v, want %v", result, expect)
+	type args struct {
+		array interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want interface{}
+	}{
+		{
+			name: "int array",
+			args: args{array: []int{1, 2, 3, 4, 5}},
+			want: []int{5, 4, 3, 2, 1},
+		},
+		{
+			name: "float array",
+			args: args{array: []float64{1, 2, 3, 4, 5}},
+			want: []float64{5, 4, 3, 2, 1},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ReverseSortArray(tt.args.array); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ReverseSortArray() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
 func TestUniqueArray(t *testing.T) {
-	array := []int{1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 5}
-	result := UniqueArray(ToFloat64(array))
-	expect := []float64{1, 2, 3, 4, 5}
+	type args struct {
+		array interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want interface{}
+	}{
+		{
+			name: "int array",
+			args: args{array: []int{1, 2, 3, 4, 5, 1, 2, 3}},
+			want: []int{1, 2, 3, 4, 5},
+		},
+		{
+			name: "float array",
+			args: args{array: []float64{1, 2, 3, 4, 5, 1, 2, 3}},
+			want: []float64{1, 2, 3, 4, 5},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := UniqueArray(tt.args.array); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("UniqueArray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
 
-	if !reflect.DeepEqual(result, expect) {
-		t.Errorf("result = %v, want %v", result, expect)
+func TestMaxOfArray(t *testing.T) {
+	type args struct {
+		array interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want interface{}
+	}{
+		{
+			name: "int array",
+			args: args{array: []int{1, 2, 3, 4, 5}},
+			want: 5,
+		},
+		{
+			name: "float array",
+			args: args{array: []float64{1, 2, 3, 4, 5}},
+			want: 5.0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := MaxOfArray(tt.args.array); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("MaxOfArray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMinOfArray(t *testing.T) {
+	type args struct {
+		array interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want interface{}
+	}{
+		{
+			name: "int array",
+			args: args{array: []int{1, 2, 3, 4, 5}},
+			want: 1,
+		},
+		{
+			name: "float array",
+			args: args{array: []float64{1, 2, 3, 4, 5}},
+			want: 1.0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := MinOfArray(tt.args.array); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("MinOfArray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSumOfArray(t *testing.T) {
+	type args struct {
+		array interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want interface{}
+	}{
+		{
+			name: "int array",
+			args: args{array: []int{1, 2, 3, 4, 5}},
+			want: 15,
+		},
+		{
+			name: "float array",
+			args: args{array: []float64{1, 2, 3, 4, 5}},
+			want: 15.0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := SumOfArray(tt.args.array); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("SumOfArray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestAvarageOfArray(t *testing.T) {
+	type args struct {
+		array interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want interface{}
+	}{
+		{
+			name: "int array",
+			args: args{array: []int{1, 2, 3, 4, 5}},
+			want: 3,
+		},
+		{
+			name: "float array",
+			args: args{array: []float64{1, 2, 3, 4, 5}},
+			want: 3.0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := AvarageOfArray(tt.args.array); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("AvarageOfArray() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
