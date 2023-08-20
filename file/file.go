dac9f907544784ea11d82b990515263c152b8fc4
ritarock <dvryz1204@gmail.com>
2023-08-20 14:48:25 +0900

    update file

---
+++ file/file.go
@@ -0,0 +1,36 @@
+package file
+
+import (
+	"io"
+	"os"
+)
+
+func Read(path string) (string, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	b, err := io.ReadAll(f)
+	if err != nil {
+		return "", err
+	}
+
+	return string(b), nil
+}
+
+func Write(path string, data string) error {
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	_, err = f.Write([]byte(data))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
